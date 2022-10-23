015019b6b2f8d21f606c732154c2f5bddb28e549
E'laishah Ramirez <drz0224@gmail.com>
2022-10-23 14:07:30 -0700

    final commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
+import { render } from "@testing-library/react"
 
-
-
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
